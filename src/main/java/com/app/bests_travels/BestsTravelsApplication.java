6568c9a09629be25f27301b2210e097858aac661
AndresGuty <angutryx@gmail.com>
2023-06-08 20:36:39 -0500

    Iniciando travels

---
+++ src/main/java/com/app/bests_travels/BestsTravelsApplication.java
@@ -0,0 +1,13 @@
+package com.app.bests_travels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestsTravelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BestsTravelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/bests_travels/BestsTravelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.bests_travels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestsTravelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
