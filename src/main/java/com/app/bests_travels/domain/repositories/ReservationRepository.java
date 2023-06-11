69774d85544624390082aa540ba9f70e6bf5cd48
AndresGuty <angutryx@gmail.com>
2023-06-10 21:50:02 -0500

    Editando Repository Reservation

--- src/main/java/com/app/bests_travels/domain/repositories/ReservationRepository.java
+++ src/main/java/com/app/bests_travels/domain/repositories/ReservationRepository.java
@@ -3,9 +3,12 @@ package com.app.bests_travels.domain.repositories;
 import com.app.bests_travels.domain.entities.FlyEntity;
 import com.app.bests_travels.domain.entities.ReservationEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.UUID;
+
 @Repository
-public interface ReservationRepository extends JpaRepository<ReservationEntity, Long> {
+public interface ReservationRepository extends CrudRepository<ReservationEntity, UUID> {
 
 }
