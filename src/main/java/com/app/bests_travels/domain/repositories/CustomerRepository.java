75c19681ddf9e840487eefb7a07d341973650a49
AndresGuty <angutryx@gmail.com>
2023-06-10 21:47:57 -0500

    Editando Repository Customer a CrudRepository

--- src/main/java/com/app/bests_travels/domain/repositories/CustomerRepository.java
+++ src/main/java/com/app/bests_travels/domain/repositories/CustomerRepository.java
@@ -1,11 +1,10 @@
 package com.app.bests_travels.domain.repositories;
 
 import com.app.bests_travels.domain.entities.CustomerEntity;
-import com.app.bests_travels.domain.entities.FlyEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<CustomerEntity, Long> {
+public interface CustomerRepository extends CrudRepository<CustomerEntity, Long> {
 
 }
